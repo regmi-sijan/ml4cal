3234918ccc2f738ca3057b9418722a6ed94e2327
buddhasystem <potekhin@bnl.gov>
2022-03-11 17:59:27 -0500

    Updated the notebook, corrected the map file for syntax

---
+++ testbeam/mapper.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+'''
+Create adjacency map
+'''
+
+import  numpy as np
+from numpy import loadtxt
+
+#################################
+# Load the map
+dataset = loadtxt('map.csv', delimiter=',')
+
+print(dataset)
+
+exit(0)
